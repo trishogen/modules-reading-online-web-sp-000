de0d52d37cdb21a91b41c4206142c618fa09efaf
Trish Ogen <patriciaogen@gmail.com>
2019-05-31 20:53:06 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,12 +1,8 @@
-require_relative './dance_module.rb'
-require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
 class Kid
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
-
-  include Dance
 
   attr_accessor :name
 
