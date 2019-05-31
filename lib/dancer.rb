4ed468d573adcec60b05fd84e9927886e7c9957c
Trish Ogen <patriciaogen@gmail.com>
2019-05-31 20:52:40 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,11 +1,10 @@
 require_relative './class_methods_module.rb'
+require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
-
-  include Dance
 
   attr_accessor :name
 
